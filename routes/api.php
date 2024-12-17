1d0475b2da5d8a4f6a63d2872365048aa4c510c9
Radit <220711664@students.uajy.ac.id>
2024-12-17 16:16:20 +0700

    api daftar barang (add)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProdukController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,10 +13,9 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [UserController::class, 'register']);
 
 Route::post('/login', [UserController::class, 'login']);
-use App\Http\Controllers\ProdukController;
-use Illuminate\Support\Facades\Auth;
+
 Route::get('/produk', [ProdukController::class, 'index']);
-Route::post('produk', [ProdukController::class, 'store']);
+Route::post('/produk', [ProdukController::class, 'store'])->name('daftarBarang.store');
 Route::get('produk/{id}', [ProdukController::class, 'show']);
 Route::put('produk/{id}', [ProdukController::class, 'update']);
 Route::delete('produk/{id}', [ProdukController::class, 'destroy']);
